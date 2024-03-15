e7c4d37e70d9a97a30207d12b5954661a1391c18
Steven Otieno <steveotianga8991@gmail.com>
2024-03-16 00:31:55 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let questions = document.getElementsByClassName("question");
+let contentDiv = document.getElementsByClassName("content");
+let questionTab = document.getElementsByClassName("question-tab");
+let icons = document.getElementsByClassName("icon");
+
+for (let i = 0; i < questions.length; i++) {
+  questions[i].addEventListener("click", () => {
+    if (parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight) {
+      contentDiv[i].style.height = contentDiv[i].scrollHeight + "px";
+      icons[i].classList.remove("plus");
+      icons[i].classList.add("minus-icon");
+      questionTab[i].classList.add("border");
+    } else {
+      contentDiv[i].style.height = "0px";
+      icons[i].classList.remove("minus-icon");
+      icons[i].classList.add("plus");
+    }
+
+    for (let j = 0; j < contentDiv.length; j++) {
+      if (j !== i) {
+        contentDiv[j].style.height = "0px";
+        icons[j].classList.remove("minus-icon");
+        icons[j].classList.add("plus");
+        questionTab[j].classList.remove("border");
+      }
+    }
+  });
+}
